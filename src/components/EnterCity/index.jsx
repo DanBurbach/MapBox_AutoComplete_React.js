f205bfcc2d5294a4218c64fbe55f94cb1897326c
DanBurbach <dburbach1982@gmail.com>
2020-01-31 21:28:18 -0800

    generate functions to handle change of text, click of suggestion in list, and allow for selection on key down using keyCode for definition of state. Begin generation in render of EnterCity

--- src/components/EnterCity/index.jsx
+++ src/components/EnterCity/index.jsx
@@ -15,23 +15,66 @@ class EnterCity extends Component {
   }
 
   onChange = event => {
+    const { recommendations } = this.props;
+    const userInput = event.currentTarget.value;
 
+    const filteredSuggestions = recommendations.filter(
+      suggestion =>
+        suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    );
+    this.setState({
+      activeSuggestion: 0,
+      filteredSuggestions,
+      showSuggestions: true,
+      userInput: event.currentTarget.value
+    });
   };
 
   onClick = event => {
-
+    this.setState({
+      activeSuggestion: 0,
+      filteredSuggestions: [],
+      showSuggestions: false,
+      userInput: event.currentTarget.innerText
+    });
   };
 
   onKeyDown = event => {
+    const { activeSuggestion, filteredSuggestions } = this.state;
 
+    if (event.keyCode === 13) {
+      this.setState({
+        activeSuggestion: 0,
+        showSuggestions: false,
+        userInput: filteredSuggestions[activeSuggestion]
+      });
+    }
+    else if (event.keyCode === 38) {
+      if (activeSuggestion === 0) {
+        return;
+      }
+
+      this.setState({ activeSuggestion: activeSuggestion - 1 });
+    }
+    else if (event.keyCode === 40) {
+      if (activeSuggestion - 1 === filteredSuggestions.length)   {
+          return;
+        }
+      this.setState({ activeSuggestion: activeSuggestion + 1 });
+    }
   };
 
   render () {
     return (
       <div>
-        <input
-          type="text"
-        />
+        <Fragment>
+          <input
+            type="text"
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+            value={userInput}
+          />
+        </Fragment>
       </div>
     );
   }
