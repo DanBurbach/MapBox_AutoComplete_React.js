00e75f8ab469e2e67b15ab1d89f6ed440471a814
DanBurbach <dburbach1982@gmail.com>
2020-01-31 21:12:43 -0800

    begin EnterCity class for handling autofill

--- src/components/EnterCity/index.jsx
+++ src/components/EnterCity/index.jsx
@@ -1,17 +1,55 @@
-export default function enterCity() {
-  console.log("test");
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 
-  async () => {
-    const res = await fetch(
-      "https://coding-challenge.echoandapex.com/locations?q=pdx"
-    );
-    const json = await res.json();
-    const predictionsList = new DocumentFragment();
-    json.predictions.forEach(prediction => {
-      const item = document.createElement("p");
-      item.innerHTML = prediction.name;
-      predictionsList.appendChild(item);
-    });
-    document.body.appendChild(predictionsList);
+class EnterCity extends Component {
+  static propTypes = {
+    suggestions: PropTypes.instanceOf(Array)
   };
+  static defaultProps = {
+    suggestions: []
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  onChange = event => {
+
+  };
+
+  onClick = event => {
+
+  };
+
+  onKeyDown = event => {
+
+  };
+
+  render () {
+    return (
+      <div>
+        <input
+          type="text"
+        />
+      </div>
+    );
+  }
+
 }
+
+export default EnterCity;
+
+//   async () => {
+//     const res = await fetch(
+//       "https://coding-challenge.echoandapex.com/locations?q=pdx"
+//     );
+//     const json = await res.json();
+//     const predictionsList = new DocumentFragment();
+//     json.predictions.forEach(prediction => {
+//       const item = document.createElement("p");
+//       item.innerHTML = prediction.name;
+//       predictionsList.appendChild(item);
+//     });
+//     document.body.appendChild(predictionsList);
+//   };
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,27 +1,58 @@
 import React, { Component } from 'react';
 
-// import enterCity from '../EnterCity'
+// import EnterCity from '../EnterCity'
 import '../../styles/Main.css';
+import EnterCity from '../EnterCity';
 
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      results: []
+    };
   }
 
-render() {
+  componentDidMount() {
+    // EnterCity();
+  }
+
+  // enterCity() {
+  //   console.log("test");
+  //   async () => {
+  //     const res = await fetch(
+  //       "https://coding-challenge.echoandapex.com/locations?q=pdx"
+  //     );
+  //     const json = await res.json();
+  //     const predictionsList = new DocumentFragment();
+  //     json.predictions.forEach(prediction => {
+  //       const item = document.createElement("p");
+  //       item.innerHTML = prediction.name;
+  //       predictionsList.appendChild(item);
+  //     });
+  //     document.body.appendChild(predictionsList);
+  //   };
+  // }
+
+  render() {
     return (
-        <div className="GlobalWrapper">
-            <h3>Type a city</h3>
-            <form>
-                <div className="autocomplete">
-                    <input id="myInput" type="text" name="myCity" placeholder="City"/>
-                </div>
-                <input type="submit" 
-                onclick={''}
-                />
-            </form>
-        </div>
+      <div className="GlobalWrapper">
+        <h3>Type a city</h3>
+        <form autoComplete="off">
+          <div className="autocomplete">
+            <EnterCity 
+              suggestions={["PDX"]}/>
+            {/* <input
+              id="myInput"
+              type="text"
+              name="myCity"
+              placeholder="City"
+              required="required"
+            /> */}
+          </div>
+          <input type="submit" onclick={""} />
+        </form>
+        <div>{this.state.results}</div>
+      </div>
     );
   }
 }
