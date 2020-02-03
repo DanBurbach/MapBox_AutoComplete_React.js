489fe874ea8c54998bf9ad70452b2ff05b85ad39
DanBurbach <dburbach1982@gmail.com>
2020-02-03 00:59:34 -0800

    clean up code and remove extra un needed code

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,142 +1,24 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 
 import "../../styles/EnterCity.css";
 import '../../styles/Main.css';
 import EnterCity from '../EnterCity';
 
 class Main extends Component {
-  static propTypes = {
-    recommendations: PropTypes.instanceOf(Array)
-  };
-  static defaultProps = {
-    recommendations: []
-  };
-
   constructor(props) {
     super(props);
     this.state = {
       location: "",
-      // activeSuggestion: 0,
-      // filteredSuggestions: [],
-      // showSuggestions: false,
-      // userInput: "",
       list: []
     };
     this.handleChange = this.handleChange.bind(this);
-
-    // this.onChange = this.onChange.bind(this);
-    // this.onClick = this.onClick.bind(this);
-    // this.onKeyDown = this.onKeyDown.bind(this);
   }
 
   handleChange(event) {
     this.setState({ location: event.target.value });
   }
 
-  // async onChange(event) {
-  //   let userEntry = event.currentTarget.value;
-
-  //   const sourceLibrary = await fetch(
-  //     `https://coding-challenge.echoandapex.com/locations?q=${userEntry}`
-  //   );
-  //   const json = await sourceLibrary.json();
-  //   const predictionsList = new DocumentFragment();
-  //   json.predictions.forEach(prediction => {
-  //     const item = document.createElement("p");
-  //     item.innerHTML = prediction.name;
-  //     predictionsList.appendChild(item);
-  //   });
-  //   this.setState({ list: document.body.appendChild(predictionsList) });
-
-  //   const { recommendations } = this.props;
-  //   const userInput = toString(this.state.list);
-  //   const filteredSuggestions = recommendations.filter(
-  //     suggestion =>
-  //       suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
-  //   );
-  //   const targetValue = event.persist.value;
-
-  //   this.setState({
-  //     activeSuggestion: 0,
-  //     filteredSuggestions,
-  //     showSuggestions: true,
-  //     userInput: targetValue
-  //   });
-  // }
-
-  // onClick = event => {
-  //   this.setState({
-  //     activeSuggestion: 0,
-  //     filteredSuggestions: [],
-  //     showSuggestions: false,
-  //     userInput: event.currentTarget.innerText
-  //   });
-  // };
-
-  // onKeyDown = event => {
-  //   const { activeSuggestion, filteredSuggestions } = this.state;
-
-  //   if (event.keyCode === 13) {
-  //     this.setState({
-  //       activeSuggestion: 0,
-  //       showSuggestions: false,
-  //       userInput: filteredSuggestions[activeSuggestion]
-  //     });
-  //   } else if (event.keyCode === 38) {
-  //     if (activeSuggestion === 0) {
-  //       return;
-  //     }
-  //     this.setState({ activeSuggestion: activeSuggestion - 1 });
-  //   } else if (event.keyCode === 40) {
-  //     if (activeSuggestion - 1 === filteredSuggestions.length) {
-  //       return;
-  //     }
-  //     this.setState({ activeSuggestion: activeSuggestion + 1 });
-  //   }
-  // };
-
   render() {
-    // let suggestionList;
-    // const {
-    //   onChange,
-    //   onClick,
-    //   onKeyDown,
-    //   state: {
-    //     activeSuggestion,
-    //     filteredSuggestions,
-    //     showSuggestions,
-    //     userInput
-    //   }
-    // } = this;
-
-    // if (showSuggestions && userInput) {
-    //   if (filteredSuggestions.length) {
-    //     suggestionList = (
-    //       <ul className="suggestions">
-    //         {filteredSuggestions.map((suggestion, index) => {
-    //           let className;
-
-    //           if (index === activeSuggestion) {
-    //             className = "suggestion-active";
-    //           }
-    //           return (
-    //             <li className={className} key={suggestion} onClick={onClick}>
-    //               {suggestion}
-    //             </li>
-    //           );
-    //         })}
-    //       </ul>
-    //     );
-    //   } else {
-    //     suggestionList = (
-    //       <div className="no-suggestions">
-    //         <em>We don't have any suggestions, give it another try!</em>
-    //       </div>
-    //     );
-    //   }
-    // }
-
     let locations = this.state.location;
     
     return (
@@ -146,7 +28,6 @@ class Main extends Component {
           <form autoComplete="off">
             <div className="autocomplete">
               <EnterCity
-                value=""
                 recommendations={[
                   // current recommendations that need to be replaced with the users chosen item based off list choice
                   "Alligator",
@@ -161,19 +42,6 @@ class Main extends Component {
                   "Wetlands"
                 ]}
               />
-
-              {/* <div>
-                <Fragment>
-                  <input
-                    type="text"
-                    onChange={onChange}
-                    onKeyDown={onKeyDown}
-                    value={userInput}
-                    placeholder="Enter A Location"
-                  />
-                  {suggestionList}
-                </Fragment>
-              </div> */}
             </div>
             <input type="submit" />
           </form>
