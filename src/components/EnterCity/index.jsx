db034c6ac5ea6f15076551306cea32b46084245d
DanBurbach <dburbach1982@gmail.com>
2020-02-03 14:23:34 -0800

    grey out component for future development

--- src/components/EnterCity/index.jsx
+++ src/components/EnterCity/index.jsx
@@ -1,167 +1,164 @@
-import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
+// Seperate component that might be used in the future to compartimentalize the flow and format of this app as per the main React.js concept.  ======================
 
-import '../../styles/EnterCity.css'
 
-class EnterCity extends Component {
-  static propTypes = {
-    recommendations: PropTypes.instanceOf(Array)
-  };
-  static defaultProps = {
-    recommendations: []
-  };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeSuggestion: 0,
-      filteredSuggestions: [],
-      showSuggestions: false,
-      userInput: "",
-      list: [],
-      testList: []
-    };
-    this.onChange = this.onChange.bind(this);
-    this.onClick = this.onClick.bind(this);
-    this.onKeyDown = this.onKeyDown.bind(this);
-  }
 
-  async onChange(event) {
-    // let userEntry = event.currentTarget.value;
+// import React, { Component, Fragment } from "react";
+// import PropTypes from "prop-types";
 
-    // await fetch(
-    //   `https://coding-challenge.echoandapex.com/locations?q=${userEntry}`
-    //   )
-    //   .then(response => response.json())
-    //   .then(response => {
-    //       this.setState({
-    //         testList: response.predictions.concat([response])
-    //       })
-    //   });
+// import '../../styles/EnterCity.css'
 
-    // let array1 = [this.state.testList[0].name];
-    // let array2 = [this.state.testList[1].name];
-    // let array3 = [this.state.testList[2].name];
-    // let array4 = [this.state.testList[3].name];
-    // let array5 = [this.state.testList[4].name];
+// class EnterCity extends Component {
+//   static propTypes = {
+//     recommendations: PropTypes.instanceOf(Array)
+//   };
+//   static defaultProps = {
+//     recommendations: []
+//   };
 
-    // const combinedResults = [array1, array2, array3, array4, array5];
-    // const flattenedResults = [].concat(...combinedResults);
-    // this.setState({
-    //     list: flattenedResults
-    // });
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       activeSuggestion: 0,
+//       filteredSuggestions: [],
+//       showSuggestions: false,
+//       userInput: "",
+//       list: [],
+//       testList: []
+//     };
+//     this.onChange = this.onChange.bind(this);
+//     this.onClick = this.onClick.bind(this);
+//     this.onKeyDown = this.onKeyDown.bind(this);
+//   }
 
-    const { recommendations } = this.props;
-    // const flatList = (this.state.list);
-    // const recommendations = flatList.toString();
-    // const userInput = this.state.list.toString();
-    const userInput = event.currentTarget.value;    
-    const filteredSuggestions = recommendations.filter(
-      suggestion =>
-        suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
-    );
+//   onChange = async (event) => {
+//     const userInput = event.currentTarget.value || "";
 
-    console.log(recommendations);
-    console.log(this.state.list);
+//     await fetch(
+//       `https://coding-challenge.echoandapex.com/locations?q=${userInput}`
+//     )
+//       .then(response => response.json())
+//       .then(response => {
+//         this.setState({
+//           testList: response.predictions.concat([response])
+//         });
+//       });
 
-    this.setState({
-      activeSuggestion: 0,
-      filteredSuggestions,
-      showSuggestions: true,
-      userInput: event.currentTarget.value
-    });
-  };
+//     let array1 = [this.state.testList[0].name] || "";
+//     let array2 = [this.state.testList[1].name] || "";
+//     let array3 = [this.state.testList[2].name] || "";
+//     let array4 = [this.state.testList[3].name] || "";
+//     let array5 = [this.state.testList[4].name] || "";
 
-  onClick = event => {
-    this.setState({
-      activeSuggestion: 0,
-      filteredSuggestions: [],
-      showSuggestions: false,
-      userInput: event.currentTarget.innerText
-    });
-  };
+//     const combinedResults = [array1, array2, array3, array4, array5] || "";
+//     const flattenedResults = [].concat(...combinedResults) || "";
+//     this.setState({
+//       list: flattenedResults
+//     });
 
-  onKeyDown = event => {
-    const { activeSuggestion, filteredSuggestions } = this.state;
+//     // const recommendations = this.props;
 
-    if (event.keyCode === 13) {
-      this.setState({
-        activeSuggestion: 0,
-        showSuggestions: false,
-        userInput: filteredSuggestions[activeSuggestion]
-      });
-      
-    } else if (event.keyCode === 38) {
-        if (activeSuggestion === 0) {
-          return;
-      }
-      this.setState({ activeSuggestion: activeSuggestion - 1 });
+//     const suggestions = this.state.list.filter(
+//       suggestion =>
+//         suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+//     );
+//     console.log(suggestions);
 
-    } else if (event.keyCode === 40) {
-        if (activeSuggestion - 1 === filteredSuggestions.length) {
-          return;
-      }
-      this.setState({ activeSuggestion: activeSuggestion + 1 });
-    }
-  };
+//     this.setState({
+//       activeSuggestion: 0,
+//       filteredSuggestions: suggestions,
+//       showSuggestions: true,
+//       userInput: event.persist.value
+//     });
+//   }
 
-  render() {
-    let suggestionList;
-    const {
-      onChange,
-      onClick,
-      onKeyDown,
-      state: {
-        activeSuggestion,
-        filteredSuggestions,
-        showSuggestions,
-        userInput
-      }
-    } = this;
+//   onClick = event => {
+//     this.setState({
+//       activeSuggestion: 0,
+//       filteredSuggestions: [],
+//       showSuggestions: false,
+//       userInput: event.currentTarget.innerText
+//     });
+//   };
 
-    if (showSuggestions && userInput) {
-      if (filteredSuggestions.length) {
-        suggestionList = (
-          <ul className="suggestions">
-            {filteredSuggestions.map((suggestion, index) => {
-              let className;
+//   onKeyDown = event => {
+//     const { activeSuggestion, filteredSuggestions } = this.state;
 
-              if (index === activeSuggestion) {
-                className = "suggestion-active";
-              }
-              return (
-                <li 
-                  className={className} 
-                  key={suggestion} 
-                  onClick={onClick}>
-                  {suggestion}
-                </li>
-              );
-            })}
-          </ul>
-        );
-      } else {
-        suggestionList = (
-          <div className="no-suggestions">
-            <em>We don't have any suggestions, give it another try!</em>
-          </div>
-        );
-      }
-    }
+//     if (event.keyCode === 13) {
+//       this.setState({
+//         activeSuggestion: 0,
+//         showSuggestions: false,
+//         userInput: filteredSuggestions[activeSuggestion]
+//       });
+//     } else if (event.keyCode === 38) {
+//       if (activeSuggestion === 0) {
+//         return;
+//       }
+//       this.setState({ activeSuggestion: activeSuggestion - 1 });
+//     } else if (event.keyCode === 40) {
+//       if (activeSuggestion - 1 === filteredSuggestions.length) {
+//         return;
+//       }
+//       this.setState({ activeSuggestion: activeSuggestion + 1 });
+//     }
+//   };
 
-    return (
-        <Fragment>
-          <input
-            type="text"
-            onChange={onChange}
-            onKeyDown={onKeyDown}
-            value={userInput}
-            placeholder="Enter A Location"
-          />
-          {suggestionList}
-        </Fragment>
-    );
-  }
-}
+//   render() {
+//     let suggestionList;
+//     const {
+//       onChange,
+//       onClick,
+//       onKeyDown,
+//       state: {
+//         activeSuggestion,
+//         filteredSuggestions,
+//         showSuggestions,
+//         userInput,
+//         list
+//       }
+//     } = this;
 
-export default EnterCity;
+//     if (showSuggestions && userInput) {
+//       if (filteredSuggestions.length) {
+//         suggestionList = (
+//           <ul className="suggestions">
+//             {filteredSuggestions.map((suggestion, index) => {
+//               let className;
+
+//               if (index === activeSuggestion) {
+//                 className = "suggestion-active";
+//               }
+//               return (
+//                 <li className={className} key={suggestion} onClick={onClick}>
+//                   {suggestion}
+//                 </li>
+//               );
+//             })}
+//           </ul>
+//         );
+//       } else {
+//         suggestionList = (
+//           <div className="no-suggestions">
+//             <em>We don't have any suggestions, give it another try!</em>
+//           </div>
+//         );
+//       }
+//     }
+
+//     return (
+//       <Fragment>
+//         <input
+//           type="text"
+//           onChange={onChange}
+//           onKeyDown={onKeyDown}
+//           value={userInput}
+//           placeholder="Enter A Location"
+//           recommendations={list}
+//         />
+//         {suggestionList}
+//       </Fragment>
+//     );
+//   }
+// }
+
+// export default EnterCity;
