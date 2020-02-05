65c95ac8816e495817d82bacb867c60f2194a3dc
DanBurbach <dburbach1982@gmail.com>
2020-02-04 22:48:15 -0800

    test the lat and lng location, plan for passing state of Main to props of mapboxAndReact component for use

--- src/components/mapboxAndReact/index.jsx
+++ src/components/mapboxAndReact/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import mapboxgl from "mapbox-gl";
+// import * as MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 import '../../styles/MapBox.css';
 
@@ -10,9 +11,9 @@ class MapBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 2
+      lng: -122.675,
+      lat: 45.5051,
+      zoom: 9.00
     };
   }
   componentDidMount() {
@@ -30,6 +31,15 @@ class MapBox extends Component {
         zoom: map.getZoom().toFixed(2)
       });
     });
+
+    // ACTIVATING THIS WILL CAUSE LIBRARY-SIDE AUTOCOMPLETE TO BE ACTIVE. DO NOT ACTIVATE UNLESS YOU WISH YOU BYPASS CREATOR'S ORIGINAL CODE PLAN (AKA A secondary search bar will render that uses MapBoxGeocoder to search/provide search results on typing)==========================
+
+//     map.addControl(
+//       new MapboxGeocoder({
+//         accessToken: mapboxgl.accessToken,
+//         mapboxgl: mapboxgl
+//       })
+//     );
   }
 
   render() {
