67e95e7fe2ed73b77c3493c9bb3b6914ee6f9931
DanBurbach <dburbach1982@gmail.com>
2020-02-05 01:34:02 -0800

    clean up code and re-order componentDidMount and componentDidUpdate code to not allow for error. Currently works

--- src/components/mapboxAndReact/index.jsx
+++ src/components/mapboxAndReact/index.jsx
@@ -1,45 +1,43 @@
 import React, { Component } from "react";
 import mapboxgl from "mapbox-gl";
-// import * as MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 import '../../styles/MapBox.css';
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZGJ1cmJhY2gxOTgyIiwiYSI6ImNrNjhhbXNwbzAzMWczcG56azQ2anhlcmsifQ.oIeM3Zzm_nFsu-dbACDbZg";
 
+
+
 class MapBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lng: -122.675,
-      lat: 45.5051,
-      zoom: 9.00
+      lng: '',
+      lat: '',
+      zoom: 10.00,
     };
   }
+
   componentDidMount() {
-    const map = new mapboxgl.Map({
+    const lng = -122.6750;
+    const lat = 45.5051;
+    new mapboxgl.Map({
       container: this.mapContainer,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [this.state.lng, this.state.lat],
+      center: [lng, lat],
       zoom: this.state.zoom
     });
+  }
 
-    map.on("move", () => {
-      this.setState({
-        lng: map.getCenter().lng.toFixed(4),
-        lat: map.getCenter().lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
-      });
+  componentDidUpdate(){
+    const lng = this.props.lng;
+    const lat = this.props.lat;
+    new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: this.state.zoom
     });
-
-    // ACTIVATING THIS WILL CAUSE LIBRARY-SIDE AUTOCOMPLETE TO BE ACTIVE. DO NOT ACTIVATE UNLESS YOU WISH YOU BYPASS CREATOR'S ORIGINAL CODE PLAN (AKA A secondary search bar will render that uses MapBoxGeocoder to search/provide search results on typing)==========================
-
-//     map.addControl(
-//       new MapboxGeocoder({
-//         accessToken: mapboxgl.accessToken,
-//         mapboxgl: mapboxgl
-//       })
-//     );
   }
 
   render() {
@@ -47,8 +45,7 @@ class MapBox extends Component {
       <div>
         <div className="sidebarStyle">
           <div>
-            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
-            {this.state.zoom}
+            Longitude: {this.props.lng} | Latitude: {this.props.lat}
           </div>
         </div>
         <br />
